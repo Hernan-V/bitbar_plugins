c4bfeb38ea0f460ccada67098aa2b99429fa383e
Hernan Valenzuela <lp_crawling_5@hotmail.com>
2020-10-29 23:43:41 -0300

    correccion de librerias en normenpwd.py

--- Hide/g-api-normenpwd/read_file.py
+++ Hide/g-api-normenpwd/read_file.py
@@ -14,10 +14,10 @@ import sys
 import os
 import pickle
 import os.path
+import json
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-import numpy as np
 
 print("🔒")
 print("---")
@@ -42,62 +42,80 @@ def main():
     group2 = None
     group3 = None
     chg = None
+    values = None
+    connError = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
     if os.path.exists(os.path.join(sys.path[0], 'token.pickle')):
         with open(os.path.join(sys.path[0], 'token.pickle'), 'rb') as token:
             creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+
+    try:
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    os.path.join(sys.path[0], "credentials.json"), SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open(os.path.join(sys.path[0], 'token.pickle'), 'wb') as token:
+                pickle.dump(creds, token)
+
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range=SAMPLE_RANGE_NAME).execute()
+        values = result.get('values', [])
+
+        with open(os.path.join(sys.path[0], 'backup_values.txt'), 'w') as filehandle:
+            json.dump(values, filehandle)
+
+    except Exception as e:
+        connError = True
+        with open(os.path.join(sys.path[0], 'backup_values.txt'), 'r') as filehandle:
+            values = json.load(filehandle)
+    finally:
+
+        if not values:
+            print('No data found.')
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                os.path.join(sys.path[0], "credentials.json"), SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open(os.path.join(sys.path[0], 'token.pickle'), 'wb') as token:
-            pickle.dump(creds, token)
+            # print(values)
+            #values.sort(key=lambda x:x[4])
+            for row in values:
+                chg = None
+                try:
+                    if (row[4]) == '':
+                        continue
+                    if (group0 != (row[0]) and (row[0]) != '') or (group0 == None):
+                        group0 = row[0] if (row[0] != None or row[0] != '') else '-'
+                        print('%s' % group0)
+                        chg = True
+                    if (group1 != (row[1]) and (row[1]) != '') or (group1 == None) or chg == True:
+                        #group1 = row[1]
+                        #print("--"+'%s' % (row[1]))
+                        group1 = row[1] if (row[1] != None or row[1] != '') else '-'
+                        print("--" + '%s' % group1)
+                        chg = True
+                    if (group2 != (row[2]) and (row[2]) != '') or (group2 == None) or chg == True:
+                        group2 = row[2] if (row[2] != None or row[2] != '') else '-'
+                        print("----" + '%s' % group2)
 
-    service = build('sheets', 'v4', credentials=creds)
-
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME).execute()
-    values = result.get('values', [])
-
-    if not values:
-        print('No data found.')
-    else:
-        #values.sort(key=lambda x:x[4])
-        for row in values:
-            chg = None
-            try:
-                if (row[4]) == '':
+                    group3 = row[3] if (row[3] != None or row[3] != '') else '-'
+                    print("------" + '%s' % group3 + "| trim=true, color=blue bash=" + pbcopy_path + " param1=" + (row[4]) + " terminal=false")
+                except IndexError:
                     continue
-                if (group0 != (row[0]) and (row[0]) != '') or (group0 == None):
-                    group0 = row[0] if (row[0] != None or row[0] != '') else '-'
-                    print('%s' % group0)
-                    chg = True
-                if (group1 != (row[1]) and (row[1]) != '') or (group1 == None) or chg == True:
-                    #group1 = row[1]
-                    #print("--"+'%s' % (row[1]))
-                    group1 = row[1] if (row[1] != None or row[1] != '') else '-'
-                    print("--" + '%s' % group1)
-                    chg = True
-                if (group2 != (row[2]) and (row[2]) != '') or (group2 == None) or chg == True:
-                    group2 = row[2] if (row[2] != None or row[2] != '') else '-'
-                    print("----" + '%s' % group2)
 
-                group3 = row[3] if (row[3] != None or row[3] != '') else '-'
-                print("------" + '%s' % group3 + "| trim=true, color=blue bash=" + pbcopy_path + " param1=" + (row[4]) + " terminal=false")
-            except IndexError:
-                continue
+        print("---")
+        print("Reference Sheet | trim=true, color=green bash=" + chrome_path + " param1=-u param2=https://docs.google.com/spreadsheets/d/1uosNcfw7vGXdXW0GhAD7oIS4DLwtEG4XVjCCGJVbvaA/edit?pli=1#gid=666997044 terminal=false refresh=true")
 
-    print("---")
-    print("Reference Sheet | trim=true, color=green bash=" + chrome_path + " param1=-u param2=https://docs.google.com/spreadsheets/d/1uosNcfw7vGXdXW0GhAD7oIS4DLwtEG4XVjCCGJVbvaA/edit?pli=1#gid=666997044 terminal=false refresh=true")
+        if connError == True:
+            print("---")
+            print("Connection Error | trim=true, color=red terminal=false refresh=true")
 
 
 if __name__ == '__main__':
